868798acb6245da576b3fdb0cc1c8439d18395ba
rhargreaves <kiyouta@gmail.com>
2016-05-25 22:43:58 +0100

    Add Selenium Gem

--- spec/suite_spec.rb
+++ spec/suite_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
 
 describe "Given I am a website user" do
-  it 'does something useful' do
+
+  before do
+    Capybara.current_driver = :selenium
+  end
+
+  it 'loads the page' do
+
+    visit "http://robh-spa-2016-demo-site.eu-west-1.elasticbeanstalk.com/"
     expect(true).to eq(true)
   end
 end
