97d4064a5b5b56f3c4f08c022e97ecb771eed892
rhargreaves <kiyouta@gmail.com>
2016-05-29 12:13:36 +0100

    Add failing test for load time

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,15 @@
 require 'capybara/rspec'
+
+class Stopwatch
+
+  def initialize()
+    @start = Time.now
+  end
+
+  def elapsed_time
+    now = Time.now
+    elapsed_time = now - @start
+    elapsed_time
+  end
+
+end
--- spec/suite_spec.rb
+++ spec/suite_spec.rb
@@ -7,8 +7,13 @@ describe "Given I am a website user", :type => :feature do
   end
 
   it 'loads the page' do
-
     visit "http://robh-spa-2016-demo-site.eu-west-1.elasticbeanstalk.com/"
     expect(page).to have_content 'Clouds'
+  end
+
+  it 'loads the page in under 5 seconds' do
+    stopwatch = Stopwatch.new
+    visit "http://robh-spa-2016-demo-site.eu-west-1.elasticbeanstalk.com/"
+    expect(stopwatch.elapsed_time).to be < 5
   end
 end
