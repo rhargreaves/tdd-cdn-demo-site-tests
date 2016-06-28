20b6371fd8a88364d11842c5d5b345dbcfa14eff
rhargreaves <kiyouta@gmail.com>
2016-06-28 11:59:38 +0100

    Updated complete branch

--- spec/suite_spec.rb
+++ spec/suite_spec.rb
@@ -7,7 +7,7 @@ describe "Given I am a website user", :type => :feature do
 
   @origin_url = 'http://robh-spa-2016-demo-site.eu-west-1.elasticbeanstalk.com/'
 
-  [@origin_url].each do |url|
+  [@origin_url, 'http://robh.spa2016demo.com.global.prod.fastly.net/'].each do |url|
 
     context "Url is #{url}" do
 
@@ -28,11 +28,41 @@ describe "Given I am a website user", :type => :feature do
       end
 
       it 'loads the page' do
-        # TODO
+        visit @site_url
+        expect(page).to have_content 'clouds'
+      end
+
+			it 'loads the page fully in under 2 seconds' do
+        stopwatch = Stopwatch.new
+        visit @site_url
+        expect(stopwatch.elapsed_time).to be < 2
+      end
+
+      it 'has up-to-date counter' do
+        api_counter_value = get_counter_value_from_api
+        page_counter_value = get_counter_value_from_page
+        puts "API counter is #{api_counter_value}, Page is #{page_counter_value}"
+        expect(page_counter_value).to be >= api_counter_value
       end
 
     end
 
+  end
+
+ def get_counter_value_from_api
+    api_url = 'http://robh-spa-2016-demo-site.eu-west-1.elasticbeanstalk.com/'
+    uri = URI(api_url + 'counter')
+    response = Net::HTTP.get(uri)
+    data = JSON.parse(response)
+    val = data["value"]
+    val
+  end
+
+  def get_counter_value_from_page
+    visit @site_url
+    page_counter_value = find('#counter').text
+    page_counter_value = page_counter_value.scan(/\d+/).join().to_i
+    page_counter_value
   end
 
 end
