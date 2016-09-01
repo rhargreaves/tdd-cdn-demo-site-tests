0b3994a800e5dc35d7e5e24fe27184b9213e6ae1
rhargreaves <robert.hargreaves@7digital.com>
2016-09-01 16:27:18 +0100

    More descriptive tests

--- spec/suite_spec.rb
+++ spec/suite_spec.rb
@@ -6,13 +6,16 @@ require 'selenium-webdriver'
 module Tests
 
   origin_url = 'http://robh-cdn-tdd-demo-site.eu-west-1.elasticbeanstalk.com/'
-  cdn_url = 'http://tdd-cdn-demo.roberthargreaves.net.global.prod.fastly.net/'
+  entrypoints = [{ :name => 'the origin', :url => origin_url },
+                 { :name => 'the CDN', :url => 'http://tdd-cdn-demo.roberthargreaves.net.global.prod.fastly.net/' }]
 
-  describe "Given I am a website user", :type => :feature do
+  describe "Given I am a site user", :type => :feature do
 
-    [origin_url, cdn_url].each do |url|
+    entrypoints.each do |entrypoint|
 
-      context "Url is #{url}" do
+      url = entrypoint[:url]
+
+      context "When hitting #{entrypoint[:name]}" do
 
         before do
           selenium_url = 'http://browser:4444/wd/hub'
@@ -25,25 +28,24 @@ module Tests
               desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome
             )
           end
-          @site_url = url
           Capybara.default_driver = :remote_browser
           Capybara.javascript_driver = :remote_browser
         end
 
         it 'loads the page' do
-          visit @site_url
+          visit url
           expect(page).to have_content 'clouds'
         end
 
         it 'loads the page fully in under 2 seconds' do
           stopwatch = Stopwatch.new
-          visit @site_url
+          visit url
           expect(stopwatch.elapsed_time).to be < 2
         end
 
         it 'has up-to-date counter' do
           api_counter_value = get_counter_value_from_api origin_url
-          page_counter_value = get_counter_value_from_page @site_url
+          page_counter_value = get_counter_value_from_page url
           puts "API counter is #{api_counter_value}, Page is #{page_counter_value}"
           expect(page_counter_value).to be >= api_counter_value
         end
