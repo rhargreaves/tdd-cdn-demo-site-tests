4c33246dc6108f9167e39c6a89046eb969596f0d
rhargreaves <kiyouta@gmail.com>
2016-09-14 21:04:55 +0100

    Do not test origin when testing cdn performance

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 build:
 	docker-compose build
 
-test-all:
+test:
 	docker-compose run tests
 
 test-site-loads:
@@ -12,10 +12,7 @@ test-site-perf:
 	docker-compose run tests bash -c "DO_NOT_HIT_CDN=1 rspec --tag loads --tag perf"
 
 test-cdn-perf:
-	docker-compose run tests bash -c "rspec --tag loads --tag perf"
-
-test-cdn-counter:
-	docker-compose run tests bash -c "rspec --tag loads --tag perf --tag counter"
+	docker-compose run tests bash -c "DO_NOT_HIT_ORIGIN=1 rspec --tag loads --tag perf"
 
 test-cdn:
 	docker-compose run tests bash -c "DO_NOT_HIT_ORIGIN=1 rspec"
--- spec/suite_spec.rb
+++ spec/suite_spec.rb
@@ -6,12 +6,14 @@ require 'selenium-webdriver'
 module Tests
 
   origin_url = 'http://robh-cdn-tdd-demo-site.eu-west-1.elasticbeanstalk.com/'
+  cdn_url = 'http://tdd-cdn-demo.roberthargreaves.net.global.prod.fastly.net/'
+
   entrypoints = []
   unless ENV['DO_NOT_HIT_ORIGIN']
     entrypoints.push({ :name => 'the origin', :url => origin_url })
   end
   unless ENV['DO_NOT_HIT_CDN']
-    entrypoints.push({ :name => 'the CDN', :url => 'http://tdd-cdn-demo.roberthargreaves.net.global.prod.fastly.net/' })
+    entrypoints.push({ :name => 'the CDN', :url => cdn_url })
   end
 
   describe "Given I am a site user", :type => :feature do
